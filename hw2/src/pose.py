4c0b1370bfe2f462106dec37aacdd720a6db62b4
BrotherBev <BrotherBev@b55975c8-b892-11de-b528-055f0b42e881>
2009-10-21 01:58:14 +0000

    pose.py comments

--- hw2/src/pose.py
+++ hw2/src/pose.py
@@ -1,36 +1,50 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('hw2')
-import rospy
-import tf
+#import roslib; roslib.load_manifest('hw2')
+#import rospy
+#import tf
 import math
 
 
 # class Pose
+#   contains the robot pose info (x position, y position, theta) and a weight
 class Pose:
-    def __init__(self, x, y, theta):
+    def __init__(self, x, y, theta, weight = 1.0):
         self.x = x
         self.y = y
         self.theta = theta
+        self.weight = weight
 
 
 # class PoseSet
+#   contains the list of poses
 class PoseSet:
-    def __init__(self, numPoses = 5000):
-        self.numPoses = numPoses
-        self.posesPerDim = int(numPoses ** (1.0/3.0) + 0.5)
+    # __init__
+    # parameter: numPoses = the number of poses, either a list or a total value
+    def __init__(self, numPoses = [20, 20, 10]):    # default to 20 in x and y, 10 in theta
+        if not isinstance(numPoses, list):  # if the argument is not a list (it's a number)
+            posesPerDim = int(numPoses ** (1.0/3.0) + 0.5)  # nearest perfect cube
+            self.numPoses = [posesPerDim, posesPerDim, posesPerDim] # same poses in each dim
+        else:   # otherwise, it's a list, just use the values directly
+            self.numPoses = numPoses
         self.poses = []
 
+    # initializeUniform: sets a uniform distribution of poses in a given range
+    # parameters:
+    #   xEnds   --  an [xmin, xmax] list
+    #   yEnds   --  a [ymin, ymax] list
+    #   thetaEnds   --  a [thetamin, thetamax] list, defaults to [0, 2pi]
+    # note: as it's written, the distribution includes the start pose but not the end pose
+    # this is to avoid double-counting 0 and 2pi
     def initializeUniform(self, xEnds, yEnds, thetaEnds = [0,2*math.pi]):
-        denom = 1.0 / self.posesPerDim
-        xInterval = float(xEnds[1] - xEnds[0])*denom
-        yInterval = float(yEnds[1] - yEnds[0])*denom
-        thetaInterval = float(thetaEnds[1] - thetaEnds[0])*denom
+        xInterval = float(xEnds[1] - xEnds[0]) / self.numPoses[0]
+        yInterval = float(yEnds[1] - yEnds[0]) / self.numPoses[1]
+        thetaInterval = float(thetaEnds[1] - thetaEnds[0]) / self.numPoses[2]
         x = xEnds[0]
-        for i in range(self.posesPerDim):
+        for i in range(self.numPoses[0]):
             y = yEnds[0]
-            for j in range(self.posesPerDim):
+            for j in range(self.numPoses[1]):
                 theta = thetaEnds[0]
-                for k in range(self.posesPerDim):
+                for k in range(self.numPoses[2]):
                     self.poses.append([x, y, theta])
                     theta += thetaInterval
                 y += yInterval
