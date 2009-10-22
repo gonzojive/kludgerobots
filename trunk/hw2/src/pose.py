0f6e30f7099555410958f4079f02a3c8c4da9ecb
BrotherBev <BrotherBev@b55975c8-b892-11de-b528-055f0b42e881>
2009-10-22 00:32:51 +0000

    added motionModel.py, getting invalid syntax error on pose.py

--- trunk/hw2/src/pose.py
+++ trunk/hw2/src/pose.py
@@ -4,6 +4,8 @@
 #import tf
 import math
 import statutil
+import motionModel
+
 
 # class Pose
 #   contains the robot pose info (x position, y position, theta) and a weight
@@ -20,7 +22,7 @@ class Pose:
 class PoseSet:
     # __init__
     # parameter(): numPoses = the number of poses, either a list or a total value
-    def __init__(self, viz, numPoses = [20, 20, 10]):    # default to 20 in x and y, 10 in theta
+    def __init__(self, viz, err = None, numPoses = [20, 20, 10]):    # default to 20 in x and y, 10 in theta
         if not isinstance(numPoses, list):  # if the argument is not a list (it's a number)
             posesPerDim = int(numPoses ** (1.0/3.0) + 0.5)  # nearest perfect cube
             self.numPoses = [posesPerDim, posesPerDim, posesPerDim] # same poses in each dim
@@ -28,32 +30,35 @@ class PoseSet:
             self.numPoses = numPoses
         self.poses = []
         self.viz = viz  # the Visualizer object that draws to rviz
+        self.error = err or motionModel.ErrorModel()   # with no arguments, we should get 0 variance
 
     # drawArrows(): send each pose out to rviz as an arrow
     def drawArrows(self):
         for p in self.poses:
             self.viz.vizArrow([p.x, p.y], p.theta, length = p.weight)
 
-    # prediction(): update each pose given old and new odometry readings
+    # predictionStep(): update each pose given old and new odometry readings
     # parameters:
     #   odomInitial --  an [x, y, angle] list of the previous odometry reading
     #   odomFinal   --  an [x, y, angle] list of the most recent odometry reading
     #   Note: Should we use Pose instead of a list? Not sure what format we want odometry in
-    def prediction(self, odomInitial, odomFinal):
-        [theta1, delta, theta2] = odomToMotionModel(odomInitial, odomFinal)    # convert from odom to 2 angles and a distance
+    def predictionStep(self, odomInitial, odomFinal):
+        motion = odomToMotionModel(odomInitial, odomFinal)    # convert from odom to 2 angles and a distance
         for p in self.poses:
-            # calculate the error in each movement
-            # once we get the gaussian function, use this here for each variable
-            errTheta1 = 0   # errTheta1 = gauss(var = sigTheta1*theta1 + sigDeltaOnTheta1*delta)
-            errDelta = 0    # errDelta = gauss(var = sigDelta*delta)
-            errTheta2 = 0   # errTheta2 = gauss(var = sigTheta1OnTheta2*theta1 + sigDeltaOnTheta2*delta + sigTheta2*theta2)
-            [dx, dy, dtheta] = motionModelToOdom([theta1+errTheta1, delta+errDelta, theta2+errTheta2])
+            predict = motionModel.Motion(0,0,0)
+            # calculate the error in each movement, and make a new estimated motion
+            # don't really want to take a square root every time here, can we just use the variance?
+            predict.theta1 = statutil.randomGaussian(motion.theta1, math.sqrt(self.error.theta1Variance(motion))
+            predict.delta = statutil.randomGaussian(motion.delta, math.sqrt(self.error.deltaVariance(motion))
+            predict.theta2 = statutil.randomGaussian(motion.theta2, math.sqrt(self.error.theta2Variance(motion))
+            # convert back to odometry pose
+            [dx, dy, dtheta] = motionModelToOdom(predictMotion)
             p.x += dx
             p.y += dy
             p.theta += dtheta
-            # update the weights based on the gaussian results?
+            # do we update the weights based on the gaussian results?
 
-    # initializeUniform(): sets a uniform distribution of poses in a given range
+    # initializeUniform(): sets a (deterministic) uniform distribution of poses in a given range
     # parameters:
     #   xEnds   --  an [xmin, xmax] list
     #   yEnds   --  a [ymin, ymax] list
@@ -70,7 +75,7 @@ class PoseSet:
             for j in range(self.numPoses[1]):
                 theta = thetaEnds[0]
                 for k in range(self.numPoses[2]):
-                    self.poses.append(Pose(x, y, theta))
+                    self.poses += Pose(x, y, theta)
                     theta += thetaInterval
                 y += yInterval
             x += xInterval
@@ -122,23 +127,3 @@ class PoseSet:
         for i in xrange(0, totalNumPoses):
             [x, y, theta] = statutil.randomMultivariateGaussian([xParams, yParams, thetaParams])
             yield Pose(x, y, theta)
-
-# odomToMotionModel(): converts initial and final odom readings into our motion model variables
-# parameters:
-#   odomInitial --  [x, y, angle] list of the initial odometry
-#   odomFinal   --  [x, y, angle] list of the final odometry
-def odomToMotionModel(odomInitial, odomFinal):
-    [dx, dy, da] = [odomFinal[0]-odomInitial[0], odomFinal[1]-odomInitial[1], odomFinal[2]-odomInitial[2]]    # calculate (final - initial)
-    theta1 = math.atan2(dy, dx) - odomInitial[2]  # initial turn
-    delta = math.sqrt(dx*dx + dy*dy)    # distance traveled
-    theta2 = da - theta1   # final turn
-    return [theta1, delta, theta2]
-
-# motionModelToOdom(): converts from a motion model to odometry
-# parameters:
-#   model   --  the motion model variables [theta1, distance, theta2]
-def motionModelToOdom(model):
-    dx = model[1] * math.cos(model[0])
-    dy = model[1] * math.sin(model[1])
-    da = model[0] + model[2]
-    return [dx, dy, da]
