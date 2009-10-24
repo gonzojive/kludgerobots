cd0c419023187dcbaf6c960da632f5c5a6c5b6a4
BrotherBev <BrotherBev@b55975c8-b892-11de-b528-055f0b42e881>
2009-10-24 08:37:47 +0000

    Part 1 almost done, just needs map check

---
+++ hw2/src/particleFilter.py
@@ -0,0 +1,88 @@
+import rospy
+import pose
+import threading
+import util
+import statutil
+import motionModel
+import math
+
+# Minimum values before we run the filter again
+minimumMovement = .03    # 30 mm - probably want to decrease this when we have more samples
+minimumTurn = util.d2r(10)  # 10 degrees
+
+
+class ParticleFilter(threading.Thread):
+    def __init__(self, odom, viz, err = None, fm = None):
+        threading.Thread.__init__(self) # initialize the threading package
+        self.poseSet = pose.PoseSet(viz, 30)    # 30 poses just for testing purposes
+        self.lastOdom = odom[0] + [odom[1]] # convert from [[x, y], a] to [x, y, a]
+        self.newOdom = self.lastOdom[:] # makes a deep copy
+        self.motionError = err or motionModel.MotionErrorModel()   # with no arguments, we should get 0 variance
+        self.fullMap = fm
+        self.runFilter = 0  # don't run the filter until you've moved enough
+        self.runFilterLock = threading.Lock()
+
+    # displayPoses(): draws the current poseSet to rviz
+    # may exhibit odd behavior while the filter is updating poses; see pose.py
+    def displayPoses(self):
+        self.poseSet.display()
+
+    # receiveOdom(): updates the particle filter with the newest odometry reading
+    # if the new reading is different enough, run the filter
+    # parameters:
+    #   odom -- the new odometry reading, currently in [[x, y], angle] format
+    def receiveOdom(self, odom):
+        self.runFilterLock.acquire()    # <---grab the lock--->
+        self.newOdom = odom[0] + [odom[1]]  # convert from [[x, y], a] to [x, y, a]
+        if self.runFilter == 0 and (abs(self.newOdom[0]-self.lastOdom[0])+abs(self.newOdom[1]-self.lastOdom[1]) > minimumMovement or abs(self.newOdom[2]-self.lastOdom[2]) > minimumTurn):
+            self.runFilter = 1  # run the filter next time the thread opens up
+        self.runFilterLock.release()    # <---release the lock--->
+            
+
+    # run(): the main threading call
+    # does a busy wait until needed, which is wasteful, but the alternative is to use events, where the danger is
+    # that the program can't continuously check for rospy.is_shutdown(), and may never stop executing if the main
+    # thread hits an error and unexpectedly quits. This is much safer.
+    def run(self):
+        while not rospy.is_shutdown():    # loop for the duration of the program
+            # IMPORTANT: Do we need a lock to compare a value to 0? I don't think so, but not positive
+            # a lock would slow this down a lot, and as long as the value can't go from 1 -> 0, I think we're OK
+            if self.runFilter == 0: # no need to run the filter
+                pass    # pass execution to another thread
+                continue    # when this resumes, start at the top of the thread again
+            # if we get here, we're going to run the filter
+            self.runFilterLock.acquire()    # <---grab the lock--->
+            motion = motionModel.odomToMotionModel(self.lastOdom, self.newOdom) # get the motion model
+            self.lastOdom = self.newOdom[:]  # save this new odometry
+            self.runFilter = 0  # reset the filter flag
+            self.runFilterLock.release()    #  <---release the lock--->
+            self.predictionStep(motion)
+            self.displayPoses() # poses have changed, so draw the new ones
+
+    # predictionStep(): update each pose given old and new odometry readings
+    # this function currently does no locking, so it assumes that no other thread is modifying poseSet.
+    # I think this is a valid assumption in our program, but an alternative is to create a lock object in
+    # the Pose class, so each pose can be locked individually. Are locks expensive? I don't think they are.
+    # parameters:
+    #   odomInitial --  an [x, y, angle] list of the previous odometry reading
+    #   odomFinal   --  an [x, y, angle] list of the most recent odometry reading
+    def predictionStep(self, motion):
+        for p in self.poseSet.poses:
+            predict = motionModel.Motion(0,0,0)
+            # calculate the error in each movement, and make a new estimated motion
+            # don't really want to take a square root every time here, can we just use the variance?
+            predict.theta1 = statutil.randomGaussian(motion.theta1, math.sqrt(self.motionError.theta1Variance(motion)))
+            predict.delta = statutil.randomGaussian(motion.delta, math.sqrt(self.motionError.deltaVariance(motion)))
+            predict.theta2 = statutil.randomGaussian(motion.theta2, math.sqrt(self.motionError.theta2Variance(motion)))
+            # convert back to odometry pose
+            [dx, dy, dtheta] = motionModel.motionModelToOdom(predict)
+            #rospy.loginfo("Motion: %s", predict.toStr())
+            #rospy.loginfo("Pose0: %s", p.toStr())
+            sinTheta = math.sin(p.theta)    # sin of the current pose angle
+            cosTheta = math.cos(p.theta)    # cos of the current pose angle
+            p.x += dx*cosTheta - dy*sinTheta    # rotate dx into pose frame and add
+            p.y += dx*sinTheta + dy*cosTheta    # rotate dy into pose frame and add
+            p.theta += dtheta   # theta adds linearly
+            #rospy.loginfo("Pose1: %s", p.toStr())
+        if self.fullMap:    # if we have a valid map, cull the poses that are in illegal positions
+            self.poseSet.poses = filter(lambda p: self.fullMap.inBounds(p), self.poseSet.poses)
--- hw2/src/viz.py
+++ hw2/src/viz.py
@@ -10,6 +10,7 @@ import std_msgs.msg
 import random
 import tf.transformations
 from vector import *
+import math
 
 from visualization_msgs.msg import *
 from geometry_msgs.msg import *
@@ -21,10 +22,12 @@ POINT_WIDTH = .04
 class Visualizer:
     def __init__(self):
         self.pub = rospy.Publisher("visualization_marker", Marker)
+        self.posePub = rospy.Publisher("particlecloud", PoseArray)
         self.idCounter = 400
         self.namedIdCounter = 1
         self.nameToId = {}
         self.nameToColor = {} # color is stored as a triple of (r g b)
+        self.arrowIDMax = -1
         
     def getIdForName(self, name):
         if name not in self.nameToId:
@@ -93,16 +96,27 @@ class Visualizer:
                              map(vector3d, points))        
         self.pub.publish(marker)
 
+    def deleteArrows(self):
+        rospy.loginfo("Deleting arrows")
+        marker = Marker()
+        marker.ns = "arrows"
+        marker.action = Marker.DELETE
+        for i in range(self.arrowIDMax+1):
+            marker.id = i
+            self.pub.publish(marker)
+
     def vizArrow(self, start, theta, size = [1.0, 1.0, 1.0], idNum = None, color = None, alpha = None):
         marker = Marker()   # create an empty Marker
-        marker.header.frame_id = "/base_laser"  # marker source frame
-        marker.header.stamp = rospy.Time()  # timestamp
+        marker.header.frame_id = "/odom"  # marker source frame
+        #marker.header.stamp = rospy.Time()  # timestamp - I think ros automatically adds these
         marker.ns = "arrows"    # namespace - might as well make it specific
         if idNum:   # if the caller wants to specify id numbers so the arrows last until overwritten
             marker.id = idNum
         else:   # otherwise, just give it a default number
             marker.id = self.idCounter
             self.idCounter += 1
+        if marker.id > self.arrowIDMax:
+            self.arrowIDMax = marker.id
         marker.type = Marker.ARROW  # arrow type marker
         marker.action = Marker.ADD  # adding a new marker
         # assign the marker starting position
@@ -110,11 +124,12 @@ class Visualizer:
         marker.pose.position.y = start[1]
         marker.pose.position.z = 0
         # create a quaternion by theta (from params) about the x axis (not sure why x works, but it does)
-        quat = tf.transformations.quaternion_about_axis(theta, [1, 0, 0])
-        marker.pose.orientation.w = quat[0]
-        marker.pose.orientation.x = quat[1]
-        marker.pose.orientation.y = quat[2]
-        marker.pose.orientation.z = quat[3]
+        quat = tf.transformations.quaternion_about_axis(theta, [0, 0, 1])
+        #rospy.loginfo("Quaternion: %0.2f %0.2f %0.2f %0.2f", quat[0], quat[1], quat[2], quat[3])
+        marker.pose.orientation.x = quat[0]
+        marker.pose.orientation.y = quat[1]
+        marker.pose.orientation.z = quat[2]
+        marker.pose.orientation.w = quat[3]
         # scale the marker if necessary
         marker.scale.x = size[0]
         marker.scale.y = size[1]
@@ -126,8 +141,23 @@ class Visualizer:
         marker.color.g = color[1]
         marker.color.b = color[2]
         marker.color.a = alpha or 1.0   # if alpha is defined, use that, otherwise use 1.0
-        marker.lifetime = rospy.Duration(1)
+        marker.lifetime = rospy.Duration(30) # 30 seconds
         self.pub.publish(marker)
+
+    def vizPoseArray(self, poses):
+        poseArray = PoseArray()
+        poseArray.header.frame_id = "/odom"
+        for p in poses:
+            poseArray.poses.append(Pose())
+            poseArray.poses[-1].position.x = p.x
+            poseArray.poses[-1].position.y = p.y
+            poseArray.poses[-1].position.z = 0
+            quat = tf.transformations.quaternion_about_axis(p.theta, [0, 0, 1])
+            poseArray.poses[-1].orientation.x = quat[0]
+            poseArray.poses[-1].orientation.y = quat[1]
+            poseArray.poses[-1].orientation.z = quat[2]
+            poseArray.poses[-1].orientation.w = quat[3]
+        self.posePub.publish(poseArray)
 
     def vizPoints(self, points, the_id=None):
         marker = Marker()
