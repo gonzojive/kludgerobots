0ef67cd8224d38e1c704a7f44ed0b6a7f6dec3bd
reddaly <reddaly@b55975c8-b892-11de-b528-055f0b42e881>
2009-10-29 01:04:59 +0000

    minor fixes and error reporting.  also disabled the update step for now...

--- hw2/src/statutil.py
+++ hw2/src/statutil.py
@@ -42,24 +42,47 @@ def ex():
     randomMultivariateGaussian([ (1.0, 10.0), [-5.0, 2.0], [6.0, .001]])
     return randomGaussian(0.0, 1.0)
 
+# usage:
+# [x for x in lowVarianceSample(["Common", "Uncommon"], [10.0, 1.0], 10)]
+# draws newSize # of objects from objects with corresponding weights
+# yields for each object
+# credit: http://www.google.com/codesearch/p?hl=en&sa=N&cd=2&ct=rc#D6Z80snicPI/trunk/fvision_modules/prob/src/fvision/prob/resampling.cpp&q=low%20variance%20resampling
+def lowVarianceSample2(objects, weights, newSize):
+    if newSize > 0:
+        sumWeights = float(sum(weights))
+        weights = [float(x) / sumWeights for x in weights]
+        print sum(weights)
+        invSize = 1.0 / float(newSize)
+        r = random.random() * invSize
+        i = 0
+        c = weights[0]
+        for m in xrange(0, newSize):
+            u = r + float(float(m) - 1.0 ) * invSize
+            while u > c:
+                i = i + 1
+                c = c + weights[i]
+            yield objects[i]
+        
 def lowVarianceSample(objects, samples = None):
-    if not samples:
+    if not samples or samples <= 0:
+        if samples <= 0 or len(objects) == 0:
+            raise Exception("Invalid number of samples provided.")
         samples = len(objects)
     rospy.loginfo("Num Samples: %d", samples)
     totalWeight = sum([o.weight for o in objects])
     result = []
-    offset = random.random()/samples*totalWeight
+    offset = random.random()*totalWeight/float(samples)
     increment = float(totalWeight)/float(samples)
     cumulativeWeight = objects[0].weight
     nextSample = offset
     index = 0
-    print offset, increment
     for i in range(samples):
         while nextSample > cumulativeWeight:
             index += 1
             cumulativeWeight += objects[index].weight
         nextSample += increment
         result.append(objects[index])
+    assert(samples == len(result))        
     for r in result:
-        r.weight = 1
+        r.weight = 1.0 / float(len(result))
     return result
