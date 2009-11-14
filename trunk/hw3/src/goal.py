75207e97653e1f619a040a5e1da6051981432d7c
BrotherBev <BrotherBev@b55975c8-b892-11de-b528-055f0b42e881>
2009-11-14 23:10:20 +0000

    commented goals a bit

--- trunk/hw3/src/goal.py
+++ trunk/hw3/src/goal.py
@@ -1,6 +1,8 @@
 import rospy
 import roslib
 
+# Goal class
+# contains x and y values
 class Goal:
     def __init__(self, goalX, goalY):
         self.x = goalX
@@ -8,13 +10,26 @@ class Goal:
     def toStr(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
 
+# GoalSet class
+# contains a list of all the goals, functions for adding and deleting
 class GoalSet:
+    # __init__()
+    # no arguments
     def __init__(self):
         self.goals = []
+
+    # newGoal()
+    # takes in a new Goal object and appends it to the list
     def newGoal(self, g):
         self.goals.append(g)
+
+    # deleteGoal()
+    # takes in an index, deletes that goal from the list
     def deleteGoal(self, index):
         del self.goals[index]
+
+    # goalExists()
+    # takes in a Goal objects, returns the index if it exists, -1 otherwise
     def goalExists(self, goal):
         i = 0
         for g in self.goals:
@@ -22,6 +37,9 @@ class GoalSet:
                 return i
             i += 1
         return -1
+
+    # logGoals()
+    # prints the current goals to rxconsole
     def logGoals(self):
         if( len(self.goals) > 0 ):
             rospy.loginfo("Current goals:")
