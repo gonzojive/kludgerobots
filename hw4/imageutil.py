23d46c93f9e559b9478ae9e97f762c6f70c58d2a
chetan.kalyan <chetan.kalyan@b55975c8-b892-11de-b528-055f0b42e881>
2009-12-07 19:45:10 +0000

    added code to draw circles and arrows

--- hw4/imageutil.py
+++ hw4/imageutil.py
@@ -1,11 +1,15 @@
 import PIL
 import PIL.Image
+import PIL.ImageDraw
+from PIL import Image,ImageDraw
 #import matplotlib
 from pylab import randn, hist
+from pylab import *
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
-
+import util
 import numpy
 #import scipy.io
     
@@ -20,6 +24,55 @@ def showImageRowCol(data, width, height):
     im.putdata(dataScaled)
     im.show()
 
+    
+# outputs a color image, scaling the data according to its maximum
+def showColorImageRowCol(data, width, height):
+    im = PIL.Image.new('RGB', (width,height))  # 'P' for palettized
+    maxData = max(data)
+    if maxData > 0:
+        dataScaled = [int(float(d) / float(maxData) * 255.0) for d in data]
+    else:
+        dataScaled = [0] * len(data)
+    im.putdata(dataScaled)
+    im.show()
+    
+    
+#function to draw a circle given a center,a radius and an image to overlay on
+def showCircleAtPoint(centerx,centery,radius,imagePath = "/home/ckalyan/Desktop/cs225b_code/hw4/data/gates-full-stage.png"):#"/home/ckalyan/Desktop"):
+    im = PIL.Image.open(imagePath)
+    draw = ImageDraw.Draw(im) # Create a draw object
+    #the circle is drawn as an ellipse within a bounding box with(left,upper,right,lower) coordinates given. 
+    #convert the (center,radius) to this system now
+    #ASSUMPTION!! -  the center and the radius are within the bounds of the image!!
+    left  = centerx - radius;
+    upper = centery - radius;
+    right = centerx + radius;
+    lower = centery + radius;
+    draw.ellipse((left,upper,right,lower), fill=(255,0,0)) # Draw a circle
+    im.show()
+    print "drawing circle"
+
+
+def showArrowAtPointAndAngle(x,y,angle,imagePath = "/home/ckalyan/Desktop/cs225b_code/hw4/data/gates-full-stage.png"):
+    im = PIL.Image.open(imagePath)
+    draw = ImageDraw.Draw(im)
+    #arrow starts at point(x,y) and extends 2 pixels ahead at the given angle
+    print "drawing line"
+    theta = util.d2r(angle) 
+    length = 20
+    endx = x+ length*cos(theta)
+    endy = y+ length*sin(theta)
+    draw.line((x,y,endx,endy),fill =128)
+    
+    Par = length/4
+    cosy = cos(theta)
+    siny = sin(theta)
+
+    draw.line((endx, endy, endx + int(-Par * cosy - (Par / 2.0 * siny)), endy + int(-Par * siny + (Par / 2.0 * cosy))),fill =128)
+    draw.line((endx,endy,endx + int(-Par * cosy + (Par / 2.0 * siny)),endy - int(Par / 2.0 * cosy + Par * siny )),fill =128)
+    im.show()
+
+    
 def showImageRowCol2(data, width, height):
     im = PIL.Image.new('L', (width,height))  # 'P' for palettized
     maxData = max(data)
